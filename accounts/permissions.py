9dbae43f5a76beb7ddff6572a1b171b4b4db5412
Sergey Dyudin <sergeydyudin@yandex.ru>
2022-03-16 11:03:37 +0300

    ref: Refactor permission class UserPermission

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -4,26 +4,15 @@ from rest_framework import permissions
 class UserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        MAP_ACTION_TO_CONDITIONS = {
-            'list': request.user.is_authenticated and request.user.is_staff,
-            'create': True,
-            'retrieve': True,
-            'update': True,
-            'partial_update': True,
-            'destroy': True,
-        }
-
-        return bool(MAP_ACTION_TO_CONDITIONS.get(view.action))
+        if view.action == 'list':
+            return bool(request.user.is_authenticated and request.user.is_staff)
+        return True
 
     def has_object_permission(self, request, view, obj):
         if not request.user.is_authenticated:
             return False
-
-        MAP_ACTION_TO_CONDITIONS = {
-            'retrieve': obj == request.user or request.user.is_staff,
-            'update': obj == request.user or request.user.is_staff,
-            'partial_update': obj == request.user or request.user.is_staff,
-            'destroy': request.user.is_staff,
-        }
-
-        return bool(MAP_ACTION_TO_CONDITIONS.get(view.action))
+        if view.action in ('retrieve', 'update', 'partial_update'):
+            return bool(obj == request.user or request.user.is_staff)
+        if view.action == 'destroy':
+            return bool(request.user.is_staff)
+        return False
