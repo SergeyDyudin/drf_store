31130d4c865c71b50748119efbf26f3f4b163624
Sergey Dyudin <sergeydyudin@yandex.ru>
2022-05-04 14:26:00 +0300

    Update django-ci.yml
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml
    
    Update django-ci.yml

--- drf_store/settings.py
+++ drf_store/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT'),
+        'PORT': os.getenv('DB_PORT', '5432'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD')
